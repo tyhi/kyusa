24f3326d82d106d64bb799d53d70e1088724ae26
Tyler Hill <tyhi@tyhi.rs>
2020-07-08 09:32:52 -0500

    Updated index
    Signed-off-by: Tyler Hill <tyhi@tyhi.rs>

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -5,10 +5,7 @@ pub mod upload;
 
 #[get("")]
 pub async fn index() -> &'static str {
-    "welcome to kyusa, a fast file upload server built in rust. \n\nfile uploads limited to 95MB \
-     to allow us to use cloudflare cdn to our server. upload in parts if needed.\n\n\ncurrently \
-     you must have an account to upload but this will change. \nin the future free uploads will \
-     have smaller file retention and registered will have longer"
+    "welcome to kyusa, a fast file upload server built in rust."
 }
 
 pub fn routes() -> Scope {
